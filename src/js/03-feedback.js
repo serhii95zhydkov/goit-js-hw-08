c26fb9d03d212d2eca9e071a551de7ae4d8663c6
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-19 21:22:55 +0200

    Update task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector(".feedback-form"),
+};
+
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {};
+
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    e.preventDefault();
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+};
+
+function onLoadFormData() {
+
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedFormData) {
+        const formData = JSON.parse(savedFormData) || {email: "", message: ""};
+        const { email, message } = formData;
+        refs.form.elements.email.value = email;
+        refs.form.elements.message.value = message;
+    };
+};
+
+onLoadFormData();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
