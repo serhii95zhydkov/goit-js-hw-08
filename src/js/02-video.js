ec312cda2a91b16a5acd91a36df21002dc082d15
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-11-20 12:33:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("iframe");
+
+const player = new Player(iframe);
+
+const STORAGE_TIME_KEY = "videoplayer-current-time";
+
+player.on("play", function () {
+    console.log("played the video!");
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log("title:", title);
+});
+
+player.on("timeupdate", throttle(onSaveCurrentTime, 1000));
+
+function onSaveCurrentTime(e) {
+    localStorage.setItem(STORAGE_TIME_KEY, e.seconds);
+}; 
+
+player.setCurrentTime(localStorage.getItem(STORAGE_TIME_KEY) || 0);
